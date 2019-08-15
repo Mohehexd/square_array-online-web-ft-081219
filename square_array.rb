219be85be8520ed58803bfee18526fd6ceaa0442
Guillermo Martinez <dumblemoe@gmail.com>
2019-08-15 21:03:34 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,9 @@
 def square_array(array)
   new_array[] << array.each **2
   #{|num| new_array.push(num**num)}
-end
   return new_array
 end
+
 # new array
 # use .each
 #shovel or push values in new_array

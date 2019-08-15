564308f4835c77428399ecbf541c0748e1afe0b4
Guillermo Martinez <dumblemoe@gmail.com>
2019-08-15 21:03:08 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,11 +1,9 @@
 def square_array(array)
   new_array[] << array.each **2
   #{|num| new_array.push(num**num)}
+end
   return new_array
 end
-
 # new array
 # use .each
 #shovel or push values in new_array
-
-# so i've gone ahead and looked at the video and realized one of the steps I need to do is push or shovel I think the new values in to the new array. But i'm not sure how to set this up along with squaring it.
